d6be6fddf082bc437b948797877bebd04cdc038a
jc_mb <co@jc0.eu>
2022-12-15 12:35:44 +0100

    chore: format settings, extensions

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("@types/prettier").Options} */
+
+module.exports = {
+  /**
+   * Reference:
+   *
+   * https://github.com/JulianCataldo/web-garden/blob/develop/configs/prettier-astro.cjs
+   *
+   * */
+  ...require('webdev-configs/prettier-astro.cjs'),
+
+  useTabs: true,
+};
