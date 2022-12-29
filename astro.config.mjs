811bc8ca028178c01733b8d619183f89c1efd98a
Julian Cataldo <contact@juliancataldo.com>
2022-12-29 22:50:45 +0100

    ci: add a base url for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { ViteRsw } from 'vite-plugin-rsw';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://juliancataldo.github.io',
+
+  base: '/astro-nannou-starter',
+
   vite: {
     plugins: [ViteRsw()],
   },
